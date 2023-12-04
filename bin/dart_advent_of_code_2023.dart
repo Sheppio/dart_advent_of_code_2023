149a4a3a792f176e51d92ae55c24197a849f6337
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2023-12-04 18:05:30 +0000

    first commit

---
+++ bin/dart_advent_of_code_2023.dart
@@ -0,0 +1,5 @@
+import 'package:dart_advent_of_code_2023/dart_advent_of_code_2023.dart' as dart_advent_of_code_2023;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_advent_of_code_2023.calculate()}!');
+}
---
+++ test/dart_advent_of_code_2023_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_advent_of_code_2023/dart_advent_of_code_2023.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
