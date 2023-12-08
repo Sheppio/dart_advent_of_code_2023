2dee9b416a48fca706ed005b27e19600d00c4340
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2023-12-08 19:11:41 +0000

    Day 8 pt 2 kind of complete.  Need to find code a Least Common Multiple function for the 6 answers instead of using an online one

--- lib/day8_camel_directions.dart
+++ lib/day8_camel_directions.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:collection';
 import 'dart:io';
+import 'dart:isolate';
+import 'dart:math';
 
 class Node {
   String id;
@@ -28,12 +30,58 @@ Future<void> Day8CamelDirections() async {
     final node = Node(id: id, left: match.namedGroup('left')!, right: match.namedGroup('right')!);
     nodes[id] = node;
   }
-  print(nodes);
-  var currentNode = nodes['AAA']!;
+  //print(nodes);
+
+//Part 1
+  // var currentNode = nodes['AAA']!;
+  // var stepCount = 0;
+  // do {
+  //   final direction = directions[stepCount % directions.length];
+  //   print("Going ${direction == 'L' ? 'left' : 'right'}");
+  //   switch (direction) {
+  //     case 'L':
+  //       currentNode = nodes[currentNode.left]!;
+  //       break;
+  //     case 'R':
+  //       currentNode = nodes[currentNode.right]!;
+  //       break;
+  //     default:
+  //       throw Exception('Unexpected direction: "$direction"');
+  //   }
+  //   stepCount++;
+  //   print('step: $stepCount, currentNode: $currentNode');
+  // } while (currentNode.id != 'ZZZ');
+//End of part 1
+
+  print('Part 2');
+  print(nodes.keys.where((element) => element.endsWith('A')).join(', '));
+  print('');
+  print(nodes.keys.where((element) => element.endsWith('Z')).join(', '));
+
+  final startNodes = nodes.values.where((element) => element.id.endsWith('A')).toList();
+
+  final futures = List.generate(
+      startNodes.length,
+      (index) => Isolate.run(() async {
+            return getStepCount(startNodes[index], nodes, directions, 'index_$index');
+          }));
+
+  var stepCounts = await Future.wait(futures);
+  print(stepCounts.join(", "));
+  var answer = BigInt.from(1);
+  for (var i = 0; i < stepCounts.length; i++) {
+    answer *= BigInt.from(stepCounts[i]);
+    print(answer);
+  }
+  print('Done');
+}
+
+int getStepCount(Node startNode, Map<String, Node> nodes, String directions, String tag) {
+  var currentNode = startNode;
   var stepCount = 0;
   do {
     final direction = directions[stepCount % directions.length];
-    print("Going ${direction == 'L' ? 'left' : 'right'}");
+    //print("Going ${direction == 'L' ? 'left' : 'right'}");
     switch (direction) {
       case 'L':
         currentNode = nodes[currentNode.left]!;
@@ -45,6 +93,47 @@ Future<void> Day8CamelDirections() async {
         throw Exception('Unexpected direction: "$direction"');
     }
     stepCount++;
-    print('step: $stepCount, currentNode: $currentNode');
-  } while (currentNode.id != 'ZZZ');
+    if (stepCount % 1000000 == 0) {
+      print('tag: $tag, step: $stepCount, currentNode: $currentNode');
+      Future.delayed(Duration.zero);
+    }
+  } while (!currentNode.id.endsWith('Z'));
+  print('DONE --> tag: $tag, step: $stepCount, currentNode: $currentNode');
+  return stepCount;
 }
+
+
+
+  // print('Starting nodes: ${currentNodes.map((e) => e.id).join(', ')}');
+  // var stepCount = 0;
+  // var zEndings = 0;
+  // var zEndingsMax = 0;
+  // do {
+  //   final direction = directions[stepCount % directions.length];
+  //   //print("Going ${direction == 'L' ? 'left' : 'right'}");
+  //   switch (direction) {
+  //     case 'L':
+  //       for (int i = 0; i < currentNodes.length; i++) {
+  //         currentNodes[i] = nodes[currentNodes[i].left]!;
+  //       }
+  //       break;
+  //     case 'R':
+  //       for (int i = 0; i < currentNodes.length; i++) {
+  //         currentNodes[i] = nodes[currentNodes[i].right]!;
+  //       }
+  //       break;
+  //     default:
+  //       throw Exception('Unexpected direction: "$direction"');
+  //   }
+  //   stepCount++;
+  //   zEndings = currentNodes.where((e) => e.id.endsWith('Z')).length;
+  //   zEndingsMax = max(zEndings, zEndingsMax);
+  //   //print('step: $stepCount, zEndings: $zEndings, currentNodes: ${currentNodes.map((e) => e.id)}');
+  //   if (stepCount % 1000000 == 0) {
+  //     print(
+  //         'step: $stepCount, zEndings: $zEndings, zEndingsMax: $zEndingsMax, currentNodes: ${currentNodes.map((e) => e.id)}');
+  //     await Future.delayed(Duration.zero); // <-- gives VSC an opportunity to stop if requested
+  //   }
+  // } while (zEndings != currentNodes.length);
+  // print(
+  //     'step: $stepCount, zEndings: $zEndings, zEndingsMax: $zEndingsMax, currentNodes: ${currentNodes.map((e) => e.id)}');
